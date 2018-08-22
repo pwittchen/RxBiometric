03ce69a675acd4d3e197fb37078a43521a23dab4
Piotr Wittchen <piotr@wittchen.io>
2018-08-22 15:33:51 +0200

    adding rx methods to Preconditions

--- library/src/main/kotlin/com/github/pwittchen/rxbiometric/library/Preconditions.kt
+++ library/src/main/kotlin/com/github/pwittchen/rxbiometric/library/Preconditions.kt
@@ -3,6 +3,7 @@ package com.github.pwittchen.rxbiometric.library
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
+import io.reactivex.Single
 
 class Preconditions {
   companion object {
@@ -10,8 +11,16 @@ class Preconditions {
       return context.packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)
     }
 
+    @JvmStatic fun hasBiometricSupportRx(context: Context): Single<Boolean> {
+      return Single.just(hasBiometricSupport(context))
+    }
+
     @JvmStatic fun isAtLeastAndroidPie(): Boolean {
       return Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
+    }
+
+    @JvmStatic fun isAtLeastAndroidPieRx(): Single<Boolean> {
+      return Single.just(isAtLeastAndroidPie())
     }
   }
 }
