61b83391c7bbdd47fa71f65c54b9c793ff63f970
Piotr Wittchen <piotr@wittchen.io>
2018-08-22 11:16:12 +0200

    adding sample code of working app with Biometric Prompt (tested on Google Pixel 2 device)

--- app-kotlin/src/main/kotlin/pwittchen/github/com/rxbiometric/MainActivity.kt
+++ app-kotlin/src/main/kotlin/pwittchen/github/com/rxbiometric/MainActivity.kt
@@ -1,15 +1,78 @@
 package pwittchen.github.com.rxbiometric
 
+import android.content.DialogInterface
+import android.hardware.biometrics.BiometricPrompt
 import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.os.CancellationSignal
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.toolbar
+import kotlinx.android.synthetic.main.content_main.button
 
+//TODO: temporary solution for testing purposes to be extracted to the library
 class MainActivity : AppCompatActivity() {
 
+  @RequiresApi(28)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     setSupportActionBar(toolbar)
+
+    val prompt = BiometricPrompt
+        .Builder(this)
+        .setTitle("title")
+        .setDescription("description")
+        .setNegativeButton(
+            "cancel",
+            mainExecutor,
+            DialogInterface.OnClickListener { dialog, _ ->
+              showMessage("cancel")
+              dialog.dismiss()
+            })
+        .build()
+
+    val cancellationSignal = CancellationSignal()
+    cancellationSignal.setOnCancelListener {
+      showMessage("cancellation signal")
+    }
+
+    button.setOnClickListener {
+      prompt.authenticate(
+          cancellationSignal,
+          mainExecutor,
+          object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+              showMessage("success")
+            }
+
+            override fun onAuthenticationFailed() {
+              super.onAuthenticationFailed()
+              showMessage("fail")
+            }
+
+            override fun onAuthenticationError(
+              errorCode: Int,
+              errString: CharSequence?
+            ) {
+              super.onAuthenticationError(errorCode, errString)
+              showMessage("error")
+            }
+
+            override fun onAuthenticationHelp(
+              helpCode: Int,
+              helpString: CharSequence?
+            ) {
+              super.onAuthenticationHelp(helpCode, helpString)
+              showMessage("help")
+            }
+          })
+    }
+  }
+
+  fun showMessage(message: String) {
+    Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT)
+        .show()
   }
 
 }
