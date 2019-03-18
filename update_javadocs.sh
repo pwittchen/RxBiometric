0ebb192a71114e3afedc6dcbd9d9746d28650bf7
Piotr Wittchen <piotr@wittchen.io>
2019-03-18 21:33:14 +0100

    Update update_javadocs.sh

--- update_javadocs.sh
+++ update_javadocs.sh
@@ -6,5 +6,5 @@ git checkout gh-pages
 cp -avr library/build/javadoc/* ./
 git add -A
 git commit -m "updating JavaDoc"
-rm -rf library/build/docs
+rm -rf library/build/javadocs
 echo "JavaDoc updated"
